0968fc9f984d9601df1ce0ab861e4eab169db6fe
ali nourzad <alocersade@gmail.com>
2020-12-12 10:41:10 +0330

    Update wallhaven.sh

--- wallhaven.sh
+++ wallhaven.sh
@@ -7,5 +7,6 @@ which aria2c || sudo dnf install -y aria2
 # purity     100/110/111  (sfw/sketchy/nsfw)     
 curl -sX GET 'https://wallhaven.cc/api/v1/search?ratios=16x9&purity=100&categories=110&sorting=views' | jq .data[].path | cut -d '"' -f 2 | tee uris.txt
 #cat uris.txt
+mkdir ./wallpaper
 aria2c -d./wallpaper -j16 -c -x16 -k1M -s16 --optimize-concurrent-downloads true  -i uris.txt
 
