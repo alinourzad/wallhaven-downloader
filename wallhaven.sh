de84d6d05e36cee9b19284a39eedb104eab109a4
ali nourzad <alocersade@gmail.com>
2020-12-12 10:32:44 +0330

    added dir wallpaper for using with feh

--- wallhaven.sh
+++ wallhaven.sh
@@ -7,5 +7,5 @@ which aria2c || sudo dnf install -y aria2
 # purity     100/110/111  (sfw/sketchy/nsfw)     
 curl -sX GET 'https://wallhaven.cc/api/v1/search?ratios=16x9&purity=100&categories=110&sorting=views' | jq .data[].path | cut -d '"' -f 2 | tee uris.txt
 #cat uris.txt
-aria2c -d. -j16 -c -x16 -k1M -s16 --optimize-concurrent-downloads true  -i uris.txt
+aria2c -d./wallpaper -j16 -c -x16 -k1M -s16 --optimize-concurrent-downloads true  -i uris.txt
 
